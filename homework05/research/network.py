ea6796d97f1312ac0edc4a8ec5a114069aeaa8b3
arnmacing <93253694+arnmacing@users.noreply.github.com>
2023-02-28 23:07:53 +0300

    fix

--- homework05/research/network.py
+++ homework05/research/network.py
@@ -20,7 +20,7 @@ def ego_network(
     :param user_id: Идентификатор пользователя, для которого строится граф друзей.
     :param friends: Идентификаторы друзей, между которыми устанавливаются связи.
     """
-    friends_graph = []
+    friends_graph = [] # type: ignore
     if friends is None:
         friends_fields: tp.List[tp.Dict[str, tp.Any]] = get_friends(user_id, fields=["nickname", "is_closed, deactivate"]).items  # type: ignore
 
